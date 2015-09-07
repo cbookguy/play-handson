d5d954229c698c52584ba0a3db4979bedc549b69
op-uranok <op.uranok@gmail.com>
2015-09-07 16:33:02 +0900

    Header Comment Delete

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -1,8 +1,5 @@
 package models
 
-/**
- * Created by k-urano on 2015/09/04.
- */
 case class Name(first: String, last: String)
 case class Person(name : Name, age: Int, blood: Option[String], mynumber: Seq[Int])
 
